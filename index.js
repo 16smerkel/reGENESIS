061b316862aeb5aa3ae80a7cddd1868cd037632f
Sean Merkel <16smerkel@gmail.com>
2024-03-01 00:57:59 -0500

    purffect v1

---
+++ index.js
@@ -0,0 +1,47 @@
+const GameViewport = {
+    WIDTH: 384,
+    HEIGHT: 224,
+};
+
+window.onload = function() {
+    const canvasEl = document.querySelector('canvas');
+    const context = canvasEl.getContext('2d');
+
+    canvasEl.width = GameViewport.WIDTH;
+    canvasEl.height = GameViewport.HEIGHT;
+
+    const [fishstarBLOCK, background] = document.querySelectorAll('img');
+
+    const position = {
+        x: GameViewport.WIDTH / 2 - fishstarBLOCK.width / 2,
+        y: 110,
+    };
+
+    let velocity = 1;
+
+    function frame() {
+        position.x += velocity;
+
+        if (position.x > GameViewport.WIDTH - fishstarBLOCK.width || position.x < 0){
+            velocity =-velocity;
+        }
+
+        //context.clearRect(0, 0, GameViewport.WIDTH, GameViewport.HEIGHT);
+        context.drawImage(background, 0, 0);
+
+        context.strokeStyle = 'yellow';
+        context.moveTo(0, 0);
+        context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+        context.moveTo(GameViewport.WIDTH, 0);
+        context.lineTo(0, GameViewport.HEIGHT);
+        context.stroke();
+
+        context.drawImage(fishstarBLOCK, position.x, position.y);
+
+        window.requestAnimationFrame(frame);
+    }
+
+    window.requestAnimationFrame(frame);
+
+    console.log(context);
+}
